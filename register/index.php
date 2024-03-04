eb5849aaa512ce19f0981b0c3f6d5c16ddf213e3
avi662002 <162042718+avi662002@users.noreply.github.com>
2024-03-04 13:12:50 +0000

    Added register frontend

--- register/index.php
+++ register/index.php
@@ -59,3 +59,46 @@ if(isset($_POST['username']) && isset($_POST["password"]) && isset($_POST["confi
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Login & Registration Form</title>
+  <link rel="stylesheet" href="styleregister.css">
+</head>
+<body>
+  <div class="container">
+    <input type="checkbox" id="check">
+    <div class="login form">
+      <header>Log in </header>
+      <form action="#">
+        <input type="text" placeholder="Enter your username">
+        <input type="password" placeholder="Enter your password">
+        <input type="button" class="button" value="Login">
+      </form>
+      <div class="signup">
+        <span class="signup">Don't have an account?
+         <label for="check">Signup</label>
+        </span>
+      </div>
+    </div>
+    <div class="registration form">
+      <header>Register</header>
+      <form action="#">
+        <input type="text" placeholder="Enter your username">
+        <input type="password" placeholder="Create a password">
+        <input type="password" placeholder="Confirm your password">
+        <input type="button" class="button" value="Signup">
+      </form>
+      <div class="signup">
+        <span class="signup">Already have an account?
+         <label for="check">Login</label>
+        </span>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
