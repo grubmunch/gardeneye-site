d760795ac50b80958009dd29aa9c49192aa1b826
avi662002 <162042718+avi662002@users.noreply.github.com>
2024-03-04 13:16:24 +0000

    added front page

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FRONT PAGE</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div><h1>GardenEye </h1></div>
+    <hr>
+    <br>
+    <h2><big>Team Members</big> </h2>
+    <hr>
+    <br>
+    <h3>Avi Patel <h4>A.Patel4249@student.leedsbeckett.ac.uk</h4> <br> </h3>
+    <h3>Leon Rymer <h4>L.Rymer7666@student.leedsbeckett.ac.uk</h4> <br> </h3>
+    <h3>Anayo<h4>M.Agwunobi9738@student.leedsbeckett.ac.uk</h4> <br> </h3>
+    <img src="https://th.bing.com/th/id/OIP.WB-htsixsO5fUpSGd6oWrAHaFj?w=227&h=180&c=7&r=0&o=5&pid=1.7" alt="">
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <button><a href="Register.html"><big>Register</big></a></button>
+    <br>
+    
+    
+</body>
+</html>
