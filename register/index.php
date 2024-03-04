372a6380a569ac184ad0b511a56e346c6bf2f7af
avi662002 <162042718+avi662002@users.noreply.github.com>
2024-03-04 13:22:46 +0000

    frontend  for login

--- register/index.php
+++ register/index.php
@@ -67,24 +67,10 @@ if(isset($_POST['username']) && isset($_POST["password"]) && isset($_POST["confi
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Login & Registration Form</title>
-  <link rel="stylesheet" href="styleregister.css">
+  <link rel="stylesheet" href="../assets/css/styleregister.css">
 </head>
 <body>
   <div class="container">
-    <input type="checkbox" id="check">
-    <div class="login form">
-      <header>Log in </header>
-      <form action="#">
-        <input type="text" placeholder="Enter your username">
-        <input type="password" placeholder="Enter your password">
-        <input type="button" class="button" value="Login">
-      </form>
-      <div class="signup">
-        <span class="signup">Don't have an account?
-         <label for="check">Signup</label>
-        </span>
-      </div>
-    </div>
     <div class="registration form">
       <header>Register</header>
       <form action="#">
